aac66cbf68a74717cc53ae6abb14a650cd40aee1
Ibrahim Sel <ibrahim.sel@eteration.com>
2026-02-12 11:57:36 +0300

    chore: update package metadata

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'core'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='0.42.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',['resource/' + package_name]),
@@ -18,7 +18,7 @@ setup(
     zip_safe=True,
     maintainer='Alp SARICA',
     maintainer_email='alp.sarica@eteration.com',
-    description='Eclipse Muto Core Package',
+    description='Eclipse Muto Core - digital twin bridge that manages device registration, connectivity, telemetry, and stack state with a remote twin server',
     license='Eclipse Public License v2.0',
     tests_require=['pytest'],
     entry_points={
